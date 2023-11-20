93b3b52024cafdb579c850f8aa30c239be3072e3
nasir0500 <nasir14107@gmail.com>
2023-11-20 09:36:24 +0600

    clock is added instead of hero image and some other functionality

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -172,6 +172,33 @@ function switchToOtherColors(color){
 }
 /*===== Style Switcher End =====*/
 
+/*===== Clock Start =====*/
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+
+    // Getting hour, mins, secs from date
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+
+}
+
+setInterval(displayTime, 1000);
+
+/*===== Clock End =====*/
+
 /*===== Typing Animation =====*/
 let typed = new Typed(".typing", {
     strings: ["","Web Developer", "Web Designer", "EEE Student"],
